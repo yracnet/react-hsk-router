5041e4b052052c1e8039bf48459ab1647326f1ab
Willyams Yujra <yracnet@gmail.com>
2021-06-25 18:33:15 -0400

    -dist

--- dist/example/routes.d.ts
+++
@@ -1,6 +0,0 @@
-import { Item } from '../module';
-import { Config } from '../module/config';
-export declare const module1: Item;
-export declare const wizard1: Config;
-export declare const app: Item;
-export declare const nivel: Item;
